dcffabef7eef59f32e88c7e330c7609971417c44
Basant Karki <123530150+basant-karki@users.noreply.github.com>
2023-05-14 15:50:27 +0545

    Update state_variable.sol

--- solidity-0.8.7/state_variable.sol
+++ solidity-0.8.7/state_variable.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.7;
 contract stateVariable{
 
 // Variables that are outside of functions but inside of contract are known as the state variables
+// Also state variables are directly deployed on the blockchain
 string public stateVar = "We are Auditors";
 uint public ourGf = 0;
 
