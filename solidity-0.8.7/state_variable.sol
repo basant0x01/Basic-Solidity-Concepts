c3d0b3d202bc7123bf134cf72886df47a064b49e
Basant Karki <123530150+basant-karki@users.noreply.github.com>
2023-05-14 15:46:25 +0545

    Create state_variable.sol

---
+++ solidity-0.8.7/state_variable.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.7;
+
+contract stateVariable{
+
+// Variables that are outside of functions but inside of contract are known as the state variables
+string public stateVar = "We are Auditors";
+uint public ourGf = 0;
+
+}
