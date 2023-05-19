9f47d95f4e49585f3d9574160951e0e79de88af6
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-05-19 12:20:19 +0545

    Create data_types.sol

---
+++ solidity-0.8.7/data_types.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.7;
+
+contract DataTypes{
+
+    string public name = "basant0x01";
+    int public temprature = -12;
+    uint public age = 65;
+    address public owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    bool public isOwner = true; //  default = false
+    bytes2 public char = "ab"; // bytes5 public char = "abcde";
+    
+}
