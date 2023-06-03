b8b0f2d6f095ef0f806ab7c215d0a2957c9a2623
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-06-03 22:09:52 +0545

    Update state_variable.sol

--- solidity-0.8.7/state_variable.sol
+++ solidity-0.8.7/state_variable.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.7;
 
+// To write or update a state variable you need to send a transaction.
+// On the other hand, you can read state variables, for free, without any transaction fee
+
 contract stateVariable{
 
 // Variables that are outside of functions but inside of contract are known as the state variables
