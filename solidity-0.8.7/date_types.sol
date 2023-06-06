eb6e44815985a4f6cb7708568a180cb946ad81d3
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-06-06 14:41:52 +0545

    Create date_types.sol

---
+++ solidity-0.8.7/date_types.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract defalutValues{
+
+    // If we donot initilize the value then the variable contains the defalut value according to
+    // the data types
+
+    uint public uintDefalut; // defalut value: 0
+    int public intDefalut; // defalut value: 0
+    bool public isAdmin; // defalut value: false
+    address public owner; // defalut value: 0x0000000000000000000000000000000000000000
+    bytes public var1; // default value: 0x
+
+}
