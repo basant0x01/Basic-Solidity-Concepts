ae14efcf5caf0fdbd6aa910eaad63164e36b89dc
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-06-09 16:21:43 +0545

    Update ether-transfer_send_and_call.sol

--- solidity-0.8.7/ether-transfer_send_and_call.sol
+++ solidity-0.8.7/ether-transfer_send_and_call.sol
@@ -11,23 +11,24 @@ contract payableExample{
       bool public etherReceived;
       address payable manager = payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
 
-      //To receive balance in contract
+      //To check if the balance is receive in the contract
      function receiveEtherToContract() public payable {
            etherReceived=true;
      }
 
-      //To transfer balancec from contract to user 
+      //To [transfer] balancec from contract to user 
       function transferEtherToUser() public {
             manager.transfer(1 ether);
       }
-
+     
+      //To send balance using [send]
       //Always use bool and require to send any balances
       function sendEtherToUser() public {
             bool successfullySend = manager.send(1 ether); // 1000000000000000000 in wei
             require(successfullySend,"Ether sending failed");
       }
 
-      //To send ether using call
+      //To send ether using [call]
       function sendEhterToUserUsingCall() public {
             (bool successfullySend,) = manager.call{value:1 ether}("");
             require(successfullySend,"failed to send ether");
