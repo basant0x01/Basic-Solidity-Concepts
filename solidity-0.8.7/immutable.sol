0e95484435e8f52a1d605d95e660a822a3c31264
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-06-13 18:05:44 +0545

    Update immutable.sol

--- solidity-0.8.7/immutable.sol
+++ solidity-0.8.7/immutable.sol
@@ -4,11 +4,11 @@ pragma solidity 0.8.7;
 contract ImmutableExample{
 
       uint public constant myAge = 23; 
-      //variables with contant keyword, should always hardcode
-      //and constant variables are cannot be used inside the constructor
-      //but we can use immutable inside the constructor. Immutable and constant are similar in function
-      //their values cannot be changed once deployed.
-      //immutable accepts the dynamic and hardcoded values both
+      // variables with constant keyword, should always hardcode
+      // and constant variables are cannot be used inside the constructor
+      // but we can use immutable inside the constructor. Immutable and constant are similar in function
+      // their values cannot be changed once deployed.
+      // immutable accepts the dynamic and hardcoded values both
 
       uint public immutable age;
       address public immutable owner;
